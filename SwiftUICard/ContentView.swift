eced2e21849e6c8b00dba33f6d1477bcd76eefb1
Vadim Rufov <vadim.rufov@gs-labs.ru>
2021-08-07 19:37:43 +0300

    Initial Commit

---
+++ SwiftUICard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICard
+//
+//  Created by Вадим Руфов on 07.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICard/SwiftUICardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICardApp.swift
+//  SwiftUICard
+//
+//  Created by Вадим Руфов on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
